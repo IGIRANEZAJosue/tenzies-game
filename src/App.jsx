0d07d825a64a8d826715bdd8f9b6aaa14fd96cd3
Josue Igiraneza <igiranezaj28@gmail.com>
2024-07-29 12:52:35 +0200

    use random dice with Math.ceil

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ function App() {
       <div className="flex justify-center items-center bg-[#0B2434] p-12 h-[100vh]">
          <main className="flex justify-center items-center bg-[#F5F5F5] rounded-xl w-full max-w-[800px] h-[400px] font-inter">
             <div className="gap-6 grid grid-cols-5 grid-rows-2">
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
-               <Die value={Math.floor(Math.random() * 6 + 1)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
+               <Die value={Math.ceil(Math.random() * 6)} />
             </div>
          </main>
       </div>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 const Die = ({ value }) => {
    return (
-      <div className="flex justify-center items-center shadow-lg rounded-lg font-bold font-karla text-[#2B283A] text-3xl size-16">
-         {value}
+      <div className="flex justify-center items-center shadow-lg rounded-lg font-bold font-karla text-[#2B283A] text-3xl cursor-pointer size-16">
+         <h2>{value}</h2>
       </div>
    );
 };
