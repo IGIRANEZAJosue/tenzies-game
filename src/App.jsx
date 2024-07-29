9aac22fd87582a9c031873b58904c310e3b9116c
Josue Igiraneza <igiranezaj28@gmail.com>
2024-07-29 16:54:36 +0200

    Add "You won" functionality when all the die have been selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Die from "./components/Die";
 import { nanoid } from "nanoid";
 
 function App() {
    const [dice, setDice] = useState(allNewDice());
+   const [tenzies, setTenzies] = useState(false);
+
+   useEffect(() => {
+      const allHeld = dice.every((die) => die.isHeld);
+      const allSameValue = dice.every((die) => die.value === dice[0].value);
+
+      if (allHeld && allSameValue) {
+         setTenzies(true);
+         console.log("You won!");
+      }
+
+      console.log("Dice state changed");
+   }, [dice]);
 
    function allNewDice() {
       const newDice = [];
@@ -68,7 +81,7 @@ function App() {
 
             <button
                onClick={rollDice}
-               className="bg-[#5035FF] px-6 py-2 rounded-lg font-karla text-3xl text-white"
+               className="bg-[#5035FF] my-4 px-6 py-2 rounded-lg font-karla text-3xl text-white"
             >
                Roll
             </button>
