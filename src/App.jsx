4aa63b68e1b77edc7a27100e2a3867ab14703722
Josue Igiraneza <igiranezaj28@gmail.com>
2024-07-29 14:52:54 +0200

    Add nanoId and use object instead of value

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 function App() {
    const [dice, setDice] = useState(allNewDice());
@@ -8,14 +9,19 @@ function App() {
    function allNewDice() {
       const newDice = [];
       for (let i = 0; i < 10; i++) {
-         newDice.push(Math.ceil(Math.random() * 6));
+         newDice.push({
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid(),
+         });
       }
 
+      console.log(newDice);
       return newDice;
    }
 
    const diceElements = dice.map((die) => {
-      return <Die value={die} />;
+      return <Die value={die.value} />;
    });
 
    function rollDice() {
@@ -24,14 +30,14 @@ function App() {
 
    return (
       <div className="flex justify-center items-center bg-[#0B2434] p-12 h-[100vh]">
-         <main className="flex flex-col justify-center items-center bg-[#F5F5F5] rounded-xl w-full max-w-[800px] h-[400px] font-inter">
+         <main className="flex flex-col justify-around items-center bg-[#F5F5F5] rounded-xl w-full max-w-[800px] h-[400px] font-inter">
             <div className="gap-6 grid grid-cols-5 grid-rows-2">
                {diceElements}
             </div>
 
             <button
                onClick={rollDice}
-               className="bg-[#5035FF] mt-6 px-6 py-2 rounded-sm font-karla text-3xl text-white"
+               className="bg-[#5035FF] px-6 py-2 rounded-sm font-karla text-3xl text-white"
             >
                Roll
             </button>
